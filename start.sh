13634c438223b2f81c14c506ab5c7cca7bbe7bf2
Liliana <liliana.nu@gmail.com>
2020-01-25 18:41:05 +0100

    Add a simple start.sh file.

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "***************************************************************************"
+echo "Note: This script requires that you have yarn installed."
+echo "Read here if you don't have it: https://yarnpkg.com/lang/en/docs/install/"
+echo "***************************************************************************"
+
+yarn install
+
+yarn start
